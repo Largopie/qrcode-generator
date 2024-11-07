24c9da1a465ac38c9bea820341dfeb6b817bb196
Largopie <jae990320@kakao.com>
2024-11-08 01:01:09 +0900

    refactor: 레이아웃 수정

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -47,86 +47,115 @@ export default function Home() {
       <div className={styles.inputContainer}>
         {/* Main Options 입력 */}
 
-        <label htmlFor='centerImage'>이미지 선택</label>
-        <input id='centerImage' accept='image/*' type='file' value={imageValue} onChange={handleImageChange} />
+        <div>
+          <label htmlFor='centerImage'>이미지 선택</label>
+          <input id='centerImage' accept='image/*' type='file' value={imageValue} onChange={handleImageChange} />
+        </div>
 
-        <label htmlFor='data'>삽입할 데이터</label>
-        <input id='data' value={mainData.data} onChange={(e) => handleDataChange(e.target.value)} className={styles.inputBox} />
+        <div>
+          <label htmlFor='data'>삽입할 데이터</label>
+          <input id='data' value={mainData.data} onChange={(e) => handleDataChange(e.target.value)} className={styles.inputBox} />
+        </div>
 
-        <label htmlFor='width'>너비(width)</label>
-        <input id='width' value={String(mainData.width)} type='number' onChange={(e) => handleWidthChange(e.target.value)} className={styles.inputBox} />
+        <div>
+          <label htmlFor='width'>너비(width)</label>
+          <input id='width' value={String(mainData.width)} type='number' onChange={(e) => handleWidthChange(e.target.value)} className={styles.inputBox} />
+        </div>
 
-        <label htmlFor='height'>높이(height)</label>
-        <input id='height' value={String(mainData.height)} type='number' onChange={(e) => handleHeightChange(e.target.value)} className={styles.inputBox} />
+        <div>
+          <label htmlFor='height'>높이(height)</label>
+          <input id='height' value={String(mainData.height)} type='number' onChange={(e) => handleHeightChange(e.target.value)} className={styles.inputBox} />
+        </div>
 
-        <label htmlFor='margin'>테두리 공백(margin)</label>
-        <input
-          id='margin'
-          value={String(mainData.margin)}
-          min={0}
-          max={10000}
-          type='number'
-          onChange={(e) => handleMarginChange(e.target.value)}
-          className={styles.inputBox}
-        />
+        <div>
+          <label htmlFor='margin'>테두리 공백(margin)</label>
+          <input
+            id='margin'
+            value={String(mainData.margin)}
+            min={0}
+            max={10000}
+            type='number'
+            onChange={(e) => handleMarginChange(e.target.value)}
+            className={styles.inputBox}
+          />
+        </div>
 
         {/* Dots Options 입력 */}
-        <label htmlFor='dots-type'>Dots Type</label>
-        <select id='dots-type' onChange={(e) => handleDotsTypeChange(e.target.value as DotsType)} value={dots.type}>
-          <option value=''>None</option>
-          <option value='classy'>Classy</option>
-          <option value='classy-rounded'>Classy Rounded</option>
-          <option value='dots'>Dots</option>
-          <option value='extra-rounded'>Extra Rounded</option>
-          <option value='rounded'>Rounded</option>
-          <option value='square'>square</option>
-        </select>
+        <div>
+          <label htmlFor='dots-type'>Dots Type</label>
+          <select id='dots-type' onChange={(e) => handleDotsTypeChange(e.target.value as DotsType)} value={dots.type}>
+            <option value=''>None</option>
+            <option value='classy'>Classy</option>
+            <option value='classy-rounded'>Classy Rounded</option>
+            <option value='dots'>Dots</option>
+            <option value='extra-rounded'>Extra Rounded</option>
+            <option value='rounded'>Rounded</option>
+            <option value='square'>square</option>
+          </select>
+        </div>
 
-        <label htmlFor='dots-color'>Dots Color</label>
-        <input id='dots-color' type='color' onChange={(e) => handleDotsColorChange(e.target.value)} value={dots.color} />
+        <div>
+          <label htmlFor='dots-color'>Dots Color</label>
+          <input id='dots-color' type='color' onChange={(e) => handleDotsColorChange(e.target.value)} value={dots.color} />
+        </div>
 
         {/* Corners Square Options 입력 */}
-        <label htmlFor='corners-square-type'>Corners Square Type</label>
-        <select id='corners-square-type' onChange={(e) => handleCornersSquareTypeChange(e.target.value as CornersSquareType)} value={cornersSquare.type}>
-          <option value=''>None</option>
-          <option value='dot'>Dot</option>
-          <option value='extra-rounded'>Extra Rounded</option>
-          <option value='square'>square</option>
-        </select>
+        <div>
+          <label htmlFor='corners-square-type'>Corners Square Type</label>
+          <select id='corners-square-type' onChange={(e) => handleCornersSquareTypeChange(e.target.value as CornersSquareType)} value={cornersSquare.type}>
+            <option value=''>None</option>
+            <option value='dot'>Dot</option>
+            <option value='extra-rounded'>Extra Rounded</option>
+            <option value='square'>square</option>
+          </select>
+        </div>
 
-        <label htmlFor='corners-square-color'>Corners Square Color</label>
-        <input id='corners-square-color' type='color' onChange={(e) => handleCornersSquareColorChange(e.target.value)} value={cornersSquare.color} />
+        <div>
+          <label htmlFor='corners-square-color'>Corners Square Color</label>
+          <input id='corners-square-color' type='color' onChange={(e) => handleCornersSquareColorChange(e.target.value)} value={cornersSquare.color} />
+        </div>
 
         {/* Corners Dot Options 입력 */}
-        <label htmlFor='corners-dot-type'>Corners Dot Type</label>
-        <select id='corners-dot-type' onChange={(e) => handleCornersDotTypeChange(e.target.value as CornersDotType)} value={cornersDot.type}>
-          <option value=''>None</option>
-          <option value='dot'>Dot</option>
-          <option value='square'>square</option>
-        </select>
+        <div>
+          <label htmlFor='corners-dot-type'>Corners Dot Type</label>
+          <select id='corners-dot-type' onChange={(e) => handleCornersDotTypeChange(e.target.value as CornersDotType)} value={cornersDot.type}>
+            <option value=''>None</option>
+            <option value='dot'>Dot</option>
+            <option value='square'>square</option>
+          </select>
+        </div>
 
-        <label htmlFor='corners-dot-color'>Corners Dot Color</label>
-        <input id='corners-dot-color' type='color' onChange={(e) => handleCornersDotColorChange(e.target.value)} value={cornersDot.color} />
+        <div>
+          <label htmlFor='corners-dot-color'>Corners Dot Color</label>
+          <input id='corners-dot-color' type='color' onChange={(e) => handleCornersDotColorChange(e.target.value)} value={cornersDot.color} />
+        </div>
 
         {/* Background Options 입력 */}
-        <label htmlFor='background-color'>Background Color</label>
-        <input id='background-color' type='color' onChange={(e) => handleBackgroundColorChange(e.target.value)} value={background.color} />
+        <div>
+          <label htmlFor='background-color'>Background Color</label>
+          <input id='background-color' type='color' onChange={(e) => handleBackgroundColorChange(e.target.value)} value={background.color} />
+        </div>
 
         {/* Image Options 입력 */}
-        <label htmlFor='hide-background-dots'>Hide Background Dots</label>
-        <input
-          id='hide-background-dots'
-          type='checkbox'
-          onChange={(e) => handleHideBackgroundDotsChange(e.target.checked)}
-          checked={image.hideBackgroundDots}
-        />
+        <div>
+          <label htmlFor='hide-background-dots'>Hide Background Dots</label>
+          <input
+            id='hide-background-dots'
+            type='checkbox'
+            onChange={(e) => handleHideBackgroundDotsChange(e.target.checked)}
+            checked={image.hideBackgroundDots}
+          />
+        </div>
 
-        <label htmlFor='image-size'>Image Size</label>
-        <input id='image-size' onChange={(e) => handleImageSizeChange(e.target.value)} value={image.imageSize} />
+        <div>
+          <label htmlFor='image-size'>Image Size</label>
+          <input id='image-size' onChange={(e) => handleImageSizeChange(e.target.value)} value={image.imageSize} />
+        </div>
 
-        <label htmlFor='image-margin'>Image Margin</label>
-        <input id='image-margin' onChange={(e) => handleImageMarginChange(e.target.value)} value={image.margin} />
-
+        <div>
+          <label htmlFor='image-margin'>Image Margin</label>
+          <input id='image-margin' onChange={(e) => handleImageMarginChange(e.target.value)} value={image.margin} />
+        </div>
         <QRCodeImage qrCode={qrCode} />
       </div>
     </>
