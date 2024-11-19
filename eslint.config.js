de6d92876dd3b3c9c2c80f2d6df392d58a4301a7
Largopie <jae990320@kakao.com>
2024-11-19 20:35:04 +0900

    chore: eslint-plugin-jsx-a11y 설치 및 eslint 에러 해결

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,12 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import storybook from 'eslint-plugin-storybook';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, ...storybook.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, jsxA11y.flatConfigs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -20,7 +21,10 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/consistent-type-imports': 'error',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'import/prefer-default-export': 'off',
     },
-  }
+  },
+  ...storybook.configs['flat/recommended']
 );
