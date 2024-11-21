4de8d28dff32e60e6a74e41efe6962547eebcbfb
Largopie <jae990320@kakao.com>
2024-11-21 18:59:38 +0900

    chore: svgr 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   if (mode === 'production') {
     return {
       base: '/qrcode-generator/',
-      plugins: [react()],
+      plugins: [react(), svgr()],
     };
   } else {
     return {
-      plugins: [react()],
+      plugins: [react(), svgr()],
     };
   }
 });
