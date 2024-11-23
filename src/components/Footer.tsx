5cce4a0d36211a08724a639ea1bc4cc0c14635b3
Largopie <jae990320@gmail.com>
2024-11-24 00:34:12 +0900

    feat: hits 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,20 @@ import { colorPalette } from '../styles/colorPalette';
 const Footer = () => {
   return (
     <StyledFooter>
-      <p>
-        Copyright © 2024, <StyledLink href='https://github.com/Largopie'>@largopie(snakta)</StyledLink>. All rights
-        reserved.
-      </p>
-      <p>Email: jae990320@gmail.com</p>
+      <div>
+        <p>
+          Copyright © 2024, <StyledLink href='https://github.com/Largopie'>@largopie(snakta)</StyledLink>. All rights
+          reserved.
+        </p>
+        <p>Email: jae990320@gmail.com</p>
+      </div>
+
+      <a href='https://hits.seeyoufarm.com'>
+        <img
+          alt=''
+          src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Flargopie.github.io%2Fqrcode-generator%2F&count_bg=%23BD93F9&title_bg=%2344475A&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false'
+        />
+      </a>
     </StyledFooter>
   );
 };
@@ -17,8 +26,8 @@ export default Footer;
 
 const StyledFooter = styled.footer`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  align-items: center;
+  justify-content: space-between;
   width: 100%;
   height: 7.2rem;
   padding: 0 7rem;
